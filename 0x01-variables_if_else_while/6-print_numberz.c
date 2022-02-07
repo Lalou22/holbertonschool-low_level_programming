adcd5f9c13e5b4788bff3c9329bb2dd0e990cef2
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-02-07 09:49:32 -0600

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+/*
+ * File: 6-print_numberz.c
+ * Auth: Lalo Rdz
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ * You are not allowed to use any variable of type char
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 48;
+	while (number <= 57)
+	{
+		putchar(number);
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
