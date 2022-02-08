8f8adbe15cdb8b7f378b61ca583b72309b6055e3
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-02-08 17:15:35 -0600

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Lalo Rdz
+ */
+#include "main.h"
+/**
+ * reset_to_98 - Updates the value it points to to 98
+ * @n: Pointer to an int
+ * Description: Takes a pointer to an int as parameter and updates.
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n =  98;
+}
