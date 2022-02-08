574488369051d6bb34971bd8d6d21144e84fb15b
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-02-08 09:36:36 -0600

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+/*
+ * File: 10-add.c
+ * Auth: Lalo Rdz
+ */
+#include "main.h"
+/**
+ * add - Sums 2 Ints.
+ * @a: Int to be multipled
+ * @b: Int to be multipled
+ *
+ * Description: Write a function that adds two integers.
+ * Return: Result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
