ac0d69eaec7cd9b2017ce8bd149be870fd3668f4
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-02-09 09:39:33 -0600

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+/*
+ * File: 7-leet.c
+ * Auth: Lalo Rdz
+ */
+#include "main.h"
+/**
+ * leet - Encodes a string into 1337.
+ * @s: Pointer to string.
+ *
+ * Description: Encodes using Leetspeak 
+ * https://en.wikipedia.org/wiki/Leet
+ * Return: Encoded strings.
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
+		}
+	}
+	return (s);
+}
