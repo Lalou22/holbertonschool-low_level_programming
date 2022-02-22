e28d9e4d99ea18279e0dafdde33c996d24db6ca9
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-02-22 11:49:52 -0600

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+/*
+ * File: dog.h
+ * Auth: Lalo Rdz
+ */
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Structure that represents a dog
+ * @name: Char array (String) used for the dog's name.
+ * @age: Float value used to store the dog's age.
+ * @owner: Char array (String) used for the dog's owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
