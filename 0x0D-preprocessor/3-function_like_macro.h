031017d963a929b389a8ec41bb6cc7836aa81924
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-02-22 10:07:13 -0600

    Task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+/*
+ * File: 3-function_like_macro.h
+ * Auth: Lalo Rdz
+ */
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
