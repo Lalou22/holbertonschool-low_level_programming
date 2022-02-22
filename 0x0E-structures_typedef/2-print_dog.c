9b1cf06269aa08a83f289b1eb90af57e3bf29eaf
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-02-22 12:10:09 -0600

    Task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+/*
+ * File: 2-print_dog.h
+ * Auth: Lalo Rdz
+ */
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - Print the variables of the Struct dog.
+ * @d: Pointer to structure Dog.
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d-> name != NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+
+	if (d-> age > 0.0)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: (nil)\n");
+
+	if (d-> owner != NULL)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)\n");
+}
