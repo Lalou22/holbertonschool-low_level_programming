49c78326f8a41270c389a26cc5eae2ba2be35d2a
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-02-22 09:57:46 -0600

    Task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+/*
+ * File: 0-object_like_macro.h
+ * Auth: Lalo Rdz
+ */
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJECT_LIKE_MACRO_H_
+
+#define SIZE 1024
+
+#endif /* _0_OBJECT_LIKE_MACRO_H_ */
