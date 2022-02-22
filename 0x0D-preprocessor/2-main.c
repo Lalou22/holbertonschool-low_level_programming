c92b9d8b3fca34eddaca91b6f737f8772a7abd53
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-02-22 10:04:28 -0600

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+/*
+ * File: 2-main.c
+ * Auth: Lalo Rdz
+ */
+#include <stdio.h>
+/**
+ * main - Prints the file name.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
