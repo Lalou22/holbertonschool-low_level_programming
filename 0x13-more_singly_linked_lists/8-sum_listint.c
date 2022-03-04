e33b84969f2f0205ed9425f37d15ab9995ec6db3
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-03-04 12:21:53 -0600

    Task 8 - Fixed Betty code styling

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -6,17 +6,16 @@
 /**
  * sum_listint - Sum of all the data (n) of a listint_t linked list.
  * @head: Pointer to the header that Starts the list.
- * @index: 
  *
  * Description: Function that returns the sum of all the data (n) of a
- * listint_t linked list. If the list is empty, return 0. 
+ * listint_t linked list. If the list is empty, return 0.
  * Return: The sum of all the data in the list.
  */
 int sum_listint(listint_t *head)
 {
-        int sum = 0;
+	int sum = 0;
 
-        while (head != NULL)
+	while (head != NULL)
 	{
 		sum += head->n;
 		head = head->next;
