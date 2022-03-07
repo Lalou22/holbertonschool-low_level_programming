b2662c712b320a0ba88b15e99e482464ff2196c2
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-03-07 10:55:19 -0600

    Task 7 - Fixed Betty code styling

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,7 +7,7 @@
 /**
  * get_nodeint_at_index - Returns the nth node of a listint_t linked list.
  * @head: Pointer to the header that Starts the list.
- * @index: 
+ * @index: Nth node to be returned.
  *
  * Description: Function that returns the nth node of a listint_t linked list.
  * Where index is the index of the node, starting at 0 if the node does not
