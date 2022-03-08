eeddb28ba51e58ce5a2bbf22d410aa14952f7c77
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-03-08 12:49:19 -0600

    Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+/*
+ * File: 1-print_binary.c
+ * Auth: Lalo Rdz
+ */
+#include "main.h"
+/**
+ * print_binary - Function that prints the binary representation of a number.
+ * @n: Unsigned long int to be printed as a binary.
+ *
+ * Description: Function that prints the binary using recursion and bit shift.
+ * Not allowed to use arrays, malloc, '% operator' or '/ operator'.
+ * Return: Void.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	printf("%ld", n & 1);
+}
