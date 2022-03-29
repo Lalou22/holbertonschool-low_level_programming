da5608cb3daa711a394fedce1aca68608ff28ea5
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-03-29 10:56:45 -0600

    Task 0: Write a program that prints the PID of the parent process

--- 0x16-Simple_Shell/Start_coding_shell/0x00-PID_PPID/0-getppid.c
+++ 0x16-Simple_Shell/Start_coding_shell/0x00-PID_PPID/0-getppid.c
@@ -1,19 +1,25 @@
+/*
+ * File: 0-getppid.c
+ * Auth: Lalo Rdz
+ */
 #include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - PID
+ * main - Write a program that prints the PID of the parent process.
  *
+ * Run your program several times within the same shell.
+ * Does echo $$ print the same value? Why?
  * Return: Always 0.
  */
 int main(void)
 {
-    pid_t my_pid;
-    pid_t parent_pid;
+	pid_t my_pid;
+	pid_t parent_pid;
 
-    my_pid = getpid();
-    parent_pid = getppid();
-    printf("Pid: %u\n", my_pid);
-    printf("Parent Pid: %u\n", parent_pid);
-    return (0);
+	my_pid = getpid();
+	parent_pid = getppid();
+	printf("Pid: %u\n", my_pid);
+	printf("Parent Pid: %u\n", parent_pid);
+	return (0);
 }
