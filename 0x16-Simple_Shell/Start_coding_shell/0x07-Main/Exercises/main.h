8a18a1b4926c17c2bd5b9d51107561d3fc723933
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-03-30 04:10:15 -0600

    Create the Header File and a C program with fuctions

---
+++ 0x16-Simple_Shell/Start_coding_shell/0x07-Main/Exercises/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar(char c);
+char *_getenv(const char *name);
+int starts_with(char *haystack, const char *needle);
+char *_getpath(char *dir_);
+void print_elem(char *dir_);
+int _putchar(char c);
+
+#endif /* MAIN_H */
