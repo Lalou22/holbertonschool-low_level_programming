1c714acaa0c091528f56713530c4cc4c6ad247d5
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-03-30 02:46:21 -0600

    Task 0: printenv with environ (Modification)

--- 0x16-Simple_Shell/Start_coding_shell/0x07-Main/Exercises/0-environ.c
+++ 0x16-Simple_Shell/Start_coding_shell/0x07-Main/Exercises/0-environ.c
@@ -6,11 +6,10 @@
 #include <unistd.h>
 
 /**
- * main - fork + wait + execve
+ * main - printenv with environ
  *
- * Description: Write a program that executes the command ls -l /tmp in 5 different child processes.
- * Each child should be created by the same process (the father).
- * Wait for a child to exit before creating a new child.
+ * Description: Write a program that prints the environment using the global
+ * variable environ.
  * Return: Always 0.
  */
 int main(void)
