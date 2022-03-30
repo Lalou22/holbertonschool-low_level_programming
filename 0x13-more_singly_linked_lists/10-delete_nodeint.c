152a2208cbbbffb3c908885445ad6ed3417f46b7
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-03-30 05:33:37 -0600

    Task 10: Modifications

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = cursor->next;
 		free(cursor);
-		return(1);
+		return (1);
 	}
 	for (i = 0; i < index - 1; i++)
 	{
