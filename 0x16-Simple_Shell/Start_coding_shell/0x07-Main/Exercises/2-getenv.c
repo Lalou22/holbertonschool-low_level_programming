ce9478e9fccf41d8149a33eda0dbb2d0c1147f61
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-03-30 04:11:01 -0600

    Task 2: Modified to include the Header file

--- 0x16-Simple_Shell/Start_coding_shell/0x07-Main/Exercises/2-getenv.c
+++ 0x16-Simple_Shell/Start_coding_shell/0x07-Main/Exercises/2-getenv.c
@@ -4,9 +4,8 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-char *_getenv(const char *name);
-int starts_with(char *haystack, const char *needle);
-char *_getpath(char *dir_);
+#include <unistd.h>
+#include "main.h"
 
 /**
  * main - Write a function that gets an environment variable.
@@ -30,60 +29,3 @@ int main(void)
 	return (0);
 }
 
-/**
- * _getenv - Function that gets an environment variable.
- * @name: String with the environment variable.
- *
- * Description: Function that gets an environment variable.
- * Without using getenv.
- * Return: Environment variable, or NULL.
- */
-char *_getenv(const char *name)
-{
-	extern char **environ;
-	int i = 0;
-
-	while (environ[i])
-	{
-		if (starts_with(environ[i], name) != 0)
-			return (_getpath(environ[i]));
-		i++;
-	}
-	return (NULL);
-}
-
-/**
- * starts_with - Function that locates if the string starts with needle.
- * @haystack: Pointer to destination string.
- * @needle: Pointer to second string to be found.
- *
- * Description: Finds if the haystack starts with needle.
- * Return: 0 if not found or 1 otherwise.
- */
-int starts_with(char *haystack, const char *needle)
-{
-	while (*needle)
-	{
-		if (*haystack++ != *needle++)
-			return (0);
-	}
-	return (1);
-}
-
-/**
- * _getpath - Function that locates the = and returns remainding text.
- * @dir_: Pointer to directory file.
- *
- * Description: Write a function that locates the = and returns all the next
- * text, after the symbol.
- * Return: Located substring, or NULL.
- */
-char *_getpath(char *dir_)
-{
-	while (*dir_)
-	{
-		if (*dir_++ == '=')
-			return (dir_++);
-	}
-	return (NULL);
-}
